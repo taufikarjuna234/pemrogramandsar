2b0e289b417f2dd3e5599e7803e272eea9c8e027
taufikarjuna234 <149167211+taufikarjuna234@users.noreply.github.com>
2023-10-27 19:36:22 +0700

    Create latihan1.py

---
+++ latihan1.py
@@ -0,0 +1,35 @@
+a = 8
+b = 6
+c = 4
+print("a:",a)
+print("b:",b)
+print("c:",c)
+print("penyelesaian:",(a+b)*(a-c))
+
+
+a = 10
+b = 7
+c = 5
+print("a:",a)
+print("b:",b)
+print("c:",c)
+print("penyelesaian:",(a*b)/(a+c))
+
+
+a = 10
+b = 5
+t = 7
+print("a:",a)
+print("b:",b)
+print("t:",t) 
+print("penyelesaian:",1/2*(a+b)*t) 
+
+
+r = 7
+print("r:",r)
+print("keliling lingkaran adalah:",2*3.14*7)
+
+
+r = 9
+print("r:",r)
+print("Luas lingkaran adalah:",3.14*7*7)
