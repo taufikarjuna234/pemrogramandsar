f94c6642c7abf995dd8246fdb12744edc7c91cf2
taufikarjuna234 <149167211+taufikarjuna234@users.noreply.github.com>
2023-11-10 20:54:52 +0700

    Create Tugas_input.py

---
+++ Tugas_input.py
@@ -0,0 +1,60 @@
+#Perhitungan (a+b)(a-c) 
+a = int(input("masukkan nilai a:")) 
+b = int(input("masukkan nilai b:")) 
+c = int(input("masukkan nilai c"))
+rumus = (a+b)*(a-c)
+print("---------------------------------")
+print("Perhitungan (a+b)(a-c)")
+print("Besar Nilai a:",a)
+print("Besar Nilai b:",b)
+print("Besar Nilai c:",c)
+print("Hasil yang didapat adalah:",rumus)
+print("---------------------------------")
+
+
+#Perhitungan (a)(b)/(a+c)
+a = int(input("masukkan nilai a:")) 
+b = int(input("masukkan nilai b:")) 
+c = int(input("masukkan nilai c"))
+rumus = (a)*(b)/(a+c)
+print("---------------------------------")
+print("Perhitungan (a)(b)/(a+c)")
+print("Besar Nilai a:",a)
+print("Besar Nilai b:",b)
+print("Besar Nilai c:",c)
+print("Hasil yang didapat adalah:",rumus)
+print("---------------------------------")
+
+
+#Luas Bangun Trapesium
+a = int(input("masukkan nilai a:")) 
+b = int(input("masukkan nilai b:")) 
+t = int(input("masukkan nilai t"))
+rumus = 1/2*(a+b)*t
+print("---------------------------------")
+print("Perhitungan Luas Trapesium")
+print("Besar Nilai a:",a)
+print("Besar Nilai b:",b)
+print("Besar Nilai t:",t)
+print("Nilai Luas Trapesium:",rumus)
+print("---------------------------------")
+
+
+#Keliling Lingkaran
+r = int(input("masukkan nilai r:")) 
+rumus = 2*3.14*r
+print("---------------------------------")
+print("Perhitungan Keliling Lingkaran")
+print("Nilai jari-jari :",r)
+print("Nilai Keliling Lingkaran :",rumus)
+print("---------------------------------")
+
+
+#Luas Lingkaran
+r = int(input("masukkan nilai r:")) 
+rumus = 3.14*r*r
+print("---------------------------------")
+print("Perhitungan Luas Lingkaran")
+print("Nilai jari-jari:",r)
+print("Nilai Luas Lingkaran:",rumus)
+print("---------------------------------")
